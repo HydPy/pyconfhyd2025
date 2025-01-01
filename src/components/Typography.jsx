8b16a7cbd45dfbcd54b15ae4bcc57fe1254a02c7
Rajat Rajdeep <rajatrajdeep@outlook.com>
2025-01-01 09:28:39 +0530

    Reduce heading font size

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -8,11 +8,12 @@ export const Heading = ({
   const baseStyle = 'font-heading font-semibold';
   const responsiveStyle =
     {
-      1: 'text-5xl md:text-6xl lg:text-7xl',
-      2: 'text-4xl md:text-5xl lg:text-6xl',
+      1: 'text-4xl md:text-5xl lg:text-6xl',
+      2: 'text-3xl md:text-4xl lg:text-5xl',
       3: 'text-2xl md:text-3xl lg:text-4xl',
       4: 'text-xl md:text-2xl lg:text-3xl',
       5: 'text-lg md:text-xl lg:text-2xl',
+      6: 'text-base md:text-lg lg:text-xl',
     }[level] || '';
 
   return (
