734c136c1db287b9aaaef8127d47995fab7cc232
Rajat Rajdeep <rajatrajdeep@outlook.com>
2025-01-09 11:47:18 +0530

    Resolve comments

--- src/components/KeynoteSpeaker.jsx
+++ src/components/KeynoteSpeaker.jsx
@@ -1,24 +1,14 @@
 import Image from 'next/image';
 import { KEYNOTE_SPEAKER } from '@/speakers';
-import { Heading, Paragraph } from './Typography';
+import { Heading } from './Typography';
+import MdxLayout from '@/components/MdxLayout';
+import AnthonyShawDescription from '@/keynotes/anthonyShaw.mdx';
 
 export function KeynoteSpeaker() {
-  const {
-    name,
-    title,
-    description,
-    speakerImgUrl,
-    speakerImgAlt,
-    recognitionsLabel,
-    recognitions,
-    authorLabel,
-    author,
-    contributionsLabel,
-    contributions,
-  } = KEYNOTE_SPEAKER;
+  const { name, title, speakerImgUrl, speakerImgAlt } = KEYNOTE_SPEAKER;
   return (
     <section
-      id="organizers"
+      id="keynote-speaker"
       className="scroll-mt-20 flex flex-col items-center py-6 w-11/12 lg:w-5/6 mx-auto"
     >
       <Heading
@@ -28,12 +18,12 @@ export function KeynoteSpeaker() {
       >
         {title}
       </Heading>
-      <div className="flex flex-col gap-3 items-center lg:pl-20 lg:pr-20">
-        <div className="grid grid-cols-1 md:grid-cols-2 rounded-lg overflow-hidden">
+      <div className="flex flex-col items-center">
+        <div className="flex md:flex-row flex-col">
           {/* Left Section */}
-          <div className="space-y-8 justify-center items-center text-center md:text-left">
+          <div className="flex flex-col md:w-1/2">
             {/* Speaker Photo */}
-            <div className="relative lg:w-3xl lg:h-3xl w-2xl h-2xl">
+            <div className="md:w-100">
               <Image
                 src={speakerImgUrl}
                 alt={speakerImgAlt}
@@ -42,60 +32,21 @@ export function KeynoteSpeaker() {
                 className="w-full h-full object-cover"
               />
             </div>
-
             {/* Speaker Name */}
             <Heading
-              tagLevel={2}
+              tagLevel={3}
               level={2}
-              className="text-center my-8 text-secondary-600 dark:text-secondary-400"
+              className="text-center my-4 text-secondary-600 dark:text-secondary-400"
             >
               {name}
             </Heading>
           </div>
-
           {/* Right Section */}
-          <div className="bg-primary p-8 text-gray-50">
-            <div className="space-y-8">
-              <Paragraph className="text-gray-600 dark:text-gray-400 lg:w-4xl w-3xl">
-                {description}
-              </Paragraph>
-              {/* Fellow Recognition */}
-              <div className="text-gray-700 text-lg">
-                <div className="space-y-2">
-                  <Heading
-                    tagLevel={3}
-                    level={5}
-                    className="text-2xl font-bold mb-4"
-                  >
-                    {contributionsLabel}:
-                  </Heading>
-                  <Paragraph className="ml-6">{contributions}</Paragraph>
-                </div>
-                <div className="space-y-2">
-                  <Heading
-                    tagLevel={3}
-                    level={5}
-                    className="text-2xl font-bold mt-6 mb-4"
-                  >
-                    {recognitionsLabel}:
-                  </Heading>
-                  <ul className="list-disc ml-10">
-                    {recognitions.map((value, index) => (
-                      <li key={index}>{value}</li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="space-y-2">
-                  <Heading
-                    tagLevel={3}
-                    level={5}
-                    className="text-2xl font-bold mt-6"
-                  >
-                    {authorLabel}:
-                  </Heading>
-                  <Paragraph className="ml-6">{author}</Paragraph>
-                </div>
-              </div>
+          <div className="bg-primary dark:bg-secondary md:mt-8 text-gray-50 md:w-1/2">
+            <div className="">
+              <MdxLayout className="text-gray-600 dark:text-gray-400">
+                <AnthonyShawDescription />
+              </MdxLayout>
             </div>
           </div>
         </div>
