e71f2f711944109907af3f82c1e7ab7a87e2a715
Rajat Rajdeep <rajatrajdeep@outlook.com>
2025-01-31 23:40:49 +0530

    Add schedule

---
+++ data/schedule.js
@@ -0,0 +1,296 @@
+export const SCHEDULE = {
+  day1: {
+    title: 'Conference Day',
+    date: '22 Feb',
+    day: 'Saturday',
+    sessions: [
+      [
+        {
+          title: 'Registrations/ Breakfast',
+          time: '08:00 - 09:00 AM',
+          location: 'Registration Desk/ Cafeteria',
+        },
+      ],
+      [
+        {
+          title: 'Opening Note',
+          time: '09:00 - 09:10 AM',
+          speaker: {
+            name: 'PyConf Hyderabad Team',
+            title: 'Organizers',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title:
+            'Think, Code, Innovate: Srivardhan’s Logical Leap from Beginner to Advanced Python',
+          time: '09:10 - 09:25 AM',
+          speaker: {
+            name: 'Srivardhan',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'Keynote by Anthony Shaw',
+          time: '09:25 - 10:25 AM',
+          speaker: {
+            name: 'Anthony Shaw',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title:
+            'Python in IoT and Robotics: Backend Solutions for Connected Systems',
+          time: '10:25 - 10:55 AM',
+          speaker: {
+            name: 'Harsh Mittal',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title:
+            'From Raw to Reliable: Automated Data Validation with Great Expectations',
+          time: '10:55 - 11:25 AM',
+          speaker: {
+            name: 'Vivek Keshore',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'Break',
+          time: '11:25 - 11:40 AM',
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'Spicing Up Automation: A Taste of Python and SaltStack',
+          time: '11:40 - 12:10 PM',
+          speaker: {
+            name: 'Ria Bhatia',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: "Let's enhance our technical writing by reading PEPs",
+          time: '12:10 - 12:40 PM',
+          speaker: {
+            name: 'Sai Rahul Poruri',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'Lunch Break',
+          time: '12:40 - 01:40 PM',
+          location: 'Cafeteria',
+        },
+      ],
+      [
+        {
+          title: 'Lightning Talks',
+          time: '01:40 - 02:35 PM',
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title:
+            'Organized Chaos: Demystifying Memory Management & Garbage Collection in Python',
+          time: '02:35 - 03:05 PM',
+          speaker: {
+            name: 'Sejal Gupta',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'uv - MEGA: Make Environments Great Again',
+          time: '03:05 - 03:35 PM',
+          speaker: {
+            name: 'S Anand',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'Understanding API dispatching',
+          time: '03:35 - 04:05 PM',
+          speaker: {
+            name: 'Aditi Juneja',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'Multimodal Agents RAG Workflow',
+          time: '04:05 - 04:35 PM',
+          speaker: {
+            name: 'Tarun Jain',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'Turn your old laptop to a home server',
+          time: '04:35 - 05:10 PM',
+          speaker: {
+            name: 'Akhil Teja',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'Coffee Break',
+          time: '05:05 - 05:20 PM',
+          location: 'Main Hall',
+        },
+      ],
+      [
+        {
+          title: 'TBD',
+          time: '05:20 - 06:20 PM',
+          location: 'Cafeteria',
+        },
+      ],
+      [
+        {
+          title: 'Closing Note',
+          time: '06:20 - 06:30 PM',
+          location: 'Main Hall',
+        },
+      ],
+    ],
+  },
+  day2: {
+    title: 'Workshop Day',
+    date: '23 Feb',
+    day: 'Sunday',
+    sessions: [
+      [
+        {
+          time: '10:00 - 11:30 AM',
+          title:
+            'Mastering Test Driven Development in Django: A Comprehensive Guide with factory_boy and faker',
+          speaker: {
+            name: 'Kuldeep Pisda',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Track 1',
+        },
+        {
+          time: '10:00 - 11:30 AM',
+          title:
+            'Causal Inference & Uplift Modeling in Python: Beyond Correlation',
+          speaker: {
+            name: 'Sonu Kumar',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Track 2',
+        },
+        {
+          time: '10:00 - 11:30 AM',
+          title: 'Writing bug free python code with functional programming',
+          speaker: {
+            name: 'Siddharta Govindaraj',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Track 3',
+        },
+      ],
+      [
+        {
+          time: '11:30 - 1:00 PM',
+          title:
+            'Duckdb: In-Process Analytics Database that saves you millions and makes you look smart',
+          speaker: {
+            name: 'Ankur Gupta',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Track 1',
+        },
+        {
+          time: '11:30 - 1:00 PM',
+          title: 'Debugging Python applications like a Pro',
+          speaker: {
+            name: 'Satyam Soni',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Track 2',
+        },
+        {
+          time: '11:30 - 1:00 PM',
+          title:
+            'Unveiling the lazy execution benefit of FireDucks - A Multithreaded DataFrame Library with JIT compiler',
+          speaker: {
+            name: 'Sourav Saha',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Track 3',
+        },
+      ],
+      [
+        {
+          time: '2:00 - 3:30 PM',
+          title: 'TBD',
+          speaker: {
+            name: 'Anthony Shaw',
+            imgUrl: '/images/speakers/placeholder-1.svg',
+          },
+          location: 'Track 1',
+        },
+        {
+          time: '2:00 - 3:30 PM',
+          title: 'Snowflake Workshop',
+          location: 'Track 2',
+        },
+        {
+          time: '2:00 - 3:30 PM',
+          title: 'Distributed Computing',
+          speaker: {
+            name: 'Bhagirathi Hegde',
+            imgUrl: '/images/speakers/placeholder-2.svg',
+          },
+          location: 'Track 3',
+        },
+      ],
+      [
+        {
+          time: '3:30 - 5:00 PM',
+          title: 'Networking',
+          location: 'Workshop Venue',
+        },
+      ],
+    ],
+  },
+};
--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,14 +1,132 @@
-import ScheduleIntroMdx from '@/scheduleIntro.mdx';
-import MdxLayout from '@/components/MdxLayout';
-import { Heading } from '@/components/Typography';
+'use client';
+import { useState } from 'react';
+import { Heading, Span } from '@/components/Typography';
+import Icon from '@/components/Icon';
+import Image from 'next/image';
 
-export const metadata = {
-  title: 'Schedule',
-  description: 'Schedule for PyConf Hyderabad 2025',
-  openGraph: {
-    title: 'Schedule',
-    description: 'Schedule for PyConf Hyderabad 2025',
-  },
+import { SCHEDULE } from '@/schedule';
+
+const TimeBadge = ({ time, className = '' }) => {
+  return (
+    <div
+      className={`flex flex-row justify-center items-center px-2 py-1 rounded-md bg-secondary-600 dark:bg-secondary-700 text-gray-50 ${className}`}
+    >
+      <Icon name="Clock" size={16} />
+      <Span className="ml-2" level={5}>
+        {time}
+      </Span>
+    </div>
+  );
+};
+
+const LocationBadge = ({ location, className = '' }) => {
+  return (
+    <div
+      className={`flex flex-row justify-center items-center px-2 py-1 rounded-md bg-secondary-600 dark:bg-secondary-700 text-gray-50 ${className}`}
+    >
+      <Icon name="LocationDot" size={16} />
+      <Span className="ml-2" level={5}>
+        {location}
+      </Span>
+    </div>
+  );
+};
+
+const SpeakerCard = ({ speaker }) => {
+  return (
+    <div className="flex flex-row items-center my-1">
+      <div className="w-12 h-12 md:w-12 md:h-12">
+        <div className="relative shadow-md h-full w-full rounded-full overflow-hidden border-solid border-2 border-gray-700 dark:border-gray-300">
+          <Image
+            className="p-1 object-cover rounded-sm"
+            src={speaker.imgUrl}
+            alt={`Placeholder image of ${speaker.name}`}
+            fill
+          />
+        </div>
+      </div>
+      <div className="flex flex-col ml-2">
+        {speaker.name && (
+          <Span className="text-gray-950 dark:text-gray-50" level={4}>
+            {speaker.name}
+          </Span>
+        )}
+        {speaker.title && (
+          <Span className="text-gray-700 dark:text-gray-200" level={5}>
+            {speaker.title}
+          </Span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const Schedule = () => {
+  const [activeDay, setActiveDay] = useState('day1');
+  const { sessions } = SCHEDULE[activeDay];
+
+  return (
+    <div className="my-8 w-full">
+      <div className="flex justify-center gap-4 mb-6">
+        {Object.keys(SCHEDULE).map((day) => (
+          <button
+            key={day}
+            onClick={() => setActiveDay(day)}
+            className={`flex flex-col items-center px-2 md:px-10 py-2 border-2 border-solid border-gray-400 rounded ${
+              activeDay === day
+                ? 'bg-secondary-600 dark:bg-secondary-700 text-gray-50  dark:text-gray-50'
+                : 'text-gray-950 dark:text-gray-50'
+            }`}
+          >
+            <Span level={3} className="font-semibold">
+              {SCHEDULE[day].title}
+            </Span>
+            <Span level={4}>{SCHEDULE[day].date}</Span>
+            <Span level={5}>{SCHEDULE[day].day}</Span>
+          </button>
+        ))}
+      </div>
+      <div className="flex flex-col items-center space-y-4">
+        {sessions.map((session, index) => (
+          <div
+            key={index}
+            className={`flex flex-col md:flex-row gap-4 w-full ${session.length == 1 ? 'md:w-8/12' : ''}`}
+          >
+            {session.map((parallelSession, subIndex) => (
+              <div
+                key={subIndex}
+                className="flex-1 p-2 border-2 border-solid border-gray-400 rounded-lg"
+              >
+                <div className="flex flex-col items-center">
+                  <Span
+                    level={3}
+                    className="my-1 text-center font-semibold text-secondary-600 dark:text-secondary-400"
+                  >
+                    {parallelSession.title}
+                  </Span>
+                  {parallelSession.speaker && (
+                    <div className="flex flex-col space-y-2">
+                      <SpeakerCard speaker={parallelSession.speaker} />
+                    </div>
+                  )}
+                  <div className="flex flex-wrap justify-center">
+                    <TimeBadge
+                      className="ml-2 my-1"
+                      time={parallelSession.time}
+                    />
+                    <LocationBadge
+                      className="ml-2 my-1"
+                      location={parallelSession.location}
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default function Page() {
@@ -21,9 +139,7 @@ export default function Page() {
       >
         Schedule
       </Heading>
-      <MdxLayout>
-        <ScheduleIntroMdx />
-      </MdxLayout>
+      <Schedule />
     </section>
   );
 }
