b1ea19832967b4294be0832b74428605c22cd6db
Vamsi Krishna Kolli <vamsi@onemyle.com>
2025-02-15 11:31:18 +0530

    Rename volunteers to our-team

--- src/app/our-team/page.jsx
+++ src/app/our-team/page.jsx
@@ -5,14 +5,14 @@ import { VOLUNTEERS } from '@/volunteers';
 export default function Page() {
   return (
     <div className="py-12 w-11/12 lg:w-5/6 mx-auto">
-      {/* Volunteers Section */}
+      {/* OurTeam Section */}
       <section className="mb-16">
         <Heading
           tagLevel={3}
           level={3}
           className="text-center mb-12 text-secondary-600 dark:text-secondary-400"
         >
-          Volunteers
+          Our Team
         </Heading>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {VOLUNTEERS.map((volunteer, index) => (
